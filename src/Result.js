ddbefe59733b6fea28de0efb44effbdea011009a
Andrey Popp <8mayday@gmail.com>
2015-12-02 19:35:41 +0300

    fix(result): fix click on ios

--- src/Result.js
+++ src/Result.js
@@ -25,6 +25,7 @@ export default class Result extends React.Component {
     let {Root} = this.stylesheet;
     return (
       <Root
+        style={{cursor: 'pointer'}}
         tabIndex={-1}
         state={{focus: focus}}
         onClick={this._onClick}>
